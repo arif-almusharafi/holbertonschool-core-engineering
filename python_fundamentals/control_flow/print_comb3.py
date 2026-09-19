1f55bf6b4d6a75fb0587797eb9ec1da28081a3a4
arif.almusharafi <arif.almusharafi@gmai.com>
2026-09-19 22:20:18 +0300

    Add initial implementations for print_alphabt.py, print_comb2.py, print_comb3.py, and print_hexa.py

---
+++ python_fundamentals/control_flow/print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+for first in range(10):
+    for second in range(first + 1, 10):
+        if first == 8 and second == 9:
+            print("{}{}".format(first, second))
+        else:
+            print("{}{}".format(first, second), end=", ")
