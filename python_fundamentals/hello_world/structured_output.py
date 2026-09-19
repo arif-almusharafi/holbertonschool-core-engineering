44bf4dacea0500ba2ef1a0ec475928bc60fc7877
arif.almusharafi <arif.almusharafi@gmai.com>
2026-09-19 18:56:21 +0300

    Add structured output example in structured_output.py

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,12 @@
+Language = "Python"
+print(f"Language: { Language}")
+
+Version = 3
+print(f"Version: { Version}")
+
+Pi_approx = 3.14353
+print(f"Pi approx: { Pi_approx:.2f}")
+
+Computation_valid = 5 < 10
+print(f"Computation valid: { Computation_valid}")
+
