0032ddab8759527324747a66e56c4f6380ff81d3
arif.almusharafi <arif.almusharafi@gmai.com>
2026-09-19 22:51:28 +0300

    Add calculator module with basic arithmetic operations

---
+++ python_fundamentals/functions_modules/calculator_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Provide the basic arithmetic operations used by the toolbox."""
+
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    return int(a / b)
