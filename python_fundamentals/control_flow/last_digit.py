52ad592c93ffb024667045e0155fad26f832f8ea
arif.almusharafi <arif.almusharafi@gmai.com>
2026-09-19 20:59:39 +0300

    Fix condition in last_digit.py for clarity in number comparison

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -4,7 +4,7 @@ number = __import__('random').randint(-10000, 10000)
 
 if number % 10 > 5:
     print (f"Last digit of {number} is {number % 10} and is greater than 5")
-elif number % 10 <= 5 and number % 10 != 0:
+elif number % 10 < 6 and number % 10 != 0:
     print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
 else:
     print(f"Last digit of <number> is <digit> and is 0")
